03ca7f54860717b076bcf1d2740660711fe29e93
Arseniy Khvorov <khvorov45@gmail.com>
2020-12-02 11:57:33 +1100

    Create copy.sh

---
+++ copy.sh
@@ -0,0 +1,3 @@
+rsync -rv ../roc "/home/khvorova/ownCloud" --exclude "renv/library" --exclude ".snakemake"
+
+rsync -rv ../roc "/home/khvorova/vidrlwhoflu/Group/Arseniy" --exclude "renv/library" --exclude ".snakemake"
